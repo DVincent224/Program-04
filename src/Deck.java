c254b72ae3339fbbc2866a2b5ce5c381d8a21f3c
DVincent224 <djvincent224@gmail.com>
2022-10-08 21:15:36 -0500

    first commit

---
+++ src/Deck.java
@@ -0,0 +1,36 @@
+public class Deck {
+    private Card[] deck = new Card[52];
+    private int cardsDealt;
+
+    public Deck(){
+
+    }
+
+    public int getCardsDealt(){
+
+    }
+
+    private void setCardsDealt(int cardsDealt){
+
+    }
+
+    public boolean isEmptyDeck(){
+
+    }
+
+    public void collectAllCards(){
+
+    }
+
+    public Card dealCard(){
+
+    }
+
+    public void shuffleDeck(){
+
+    }
+
+    public void shuffleDeck(int swapCnt){
+        
+    }
+}
